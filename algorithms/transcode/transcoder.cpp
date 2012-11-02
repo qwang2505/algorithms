dc3d97ce5c934e238fbc3179a83ff06b80917c7f
qwang <qwang@ubuntu.(none)>
2012-11-02 17:54:38 +0800

    add transcoder logic.

--- algorithms/transcode/transcoder.cpp
+++ algorithms/transcode/transcoder.cpp
@@ -6,8 +6,10 @@ Transcoder::Transcoder(const string config, IDomNodeVisitor* visitor)
     m_visitor = visitor;
     // init config
     m_cfg.Init(config.c_str());
-    // init classifier
-    // init features
+    // init features, load feature extractors, etc.
+    m_feature_repository = FeatureRepository::get_instance();
+
+    // init classifiers, pass expression and feature names.
 }
 
 Transcoder::~Transcoder()
--- algorithms/transcode/transcoder.h
+++ algorithms/transcode/transcoder.h
@@ -3,6 +3,8 @@
 
 #include "feature_repository.h"
 #include "config.h"
+#include "boolean_classifier.h"
+#include "linear_classifier.h"
 
 class Transcoder
 {
@@ -19,6 +21,20 @@ private:
     // visitor to visit dom node.
     IDomNodeVisitor* m_visitor;
     Config m_cfg;
+
+    // classifiers
+    BooleanClassifier m_valid_node_classifier;
+    // TODO add list classifier
+    BooleanClassifier m_reorder_parent_classifier;
+    BooleanClassifier m_reorder_child_classifier;
+    LinearClassifier m_reorder_rating_classifier;
+    BooleanClassifier m_link_node_classifier;
+    BooleanClassifier m_valid_link_classifier;
+
+    // features
+    map<int, double> m_features;
+    // feature repository
+    FeatureRepository* m_feature_repository;
 };
 
 #endif
